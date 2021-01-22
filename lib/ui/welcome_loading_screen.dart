c051685dc7abc86729419e25290a7f7d673cdf92
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2021-01-22 19:42:42 +0100

    revert loading page

--- lib/ui/welcome_loading_screen.dart
+++ lib/ui/welcome_loading_screen.dart
@@ -4,8 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:studyme/models/app_state/app_state.dart';
 import 'package:studyme/routes.dart';
 
-import 'package:health/health.dart';
-
 class WelcomeLoadingScreen extends StatefulWidget {
   @override
   _WelcomeLoadingScreenState createState() => _WelcomeLoadingScreenState();
@@ -21,20 +19,9 @@ class _WelcomeLoadingScreenState extends State<WelcomeLoadingScreen> {
     }
   }
 
-  Future<void> fetchData() async {
-    /// Get everything from midnight until now
-
-    HealthFactory health = HealthFactory();
-
-    /// You MUST request access to the data types before reading them
-    bool accessWasGranted = await health
-        .requestAuthorization([HealthDataType.ACTIVE_ENERGY_BURNED]);
-    print(accessWasGranted);
-  }
-
   @override
   Widget build(BuildContext context) {
-    fetchData();
+    initAppState();
     return Scaffold(body: Center(child: CircularProgressIndicator()));
   }
 }
