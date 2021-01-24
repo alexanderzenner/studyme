7be1b6d1cde52e2de6869f46e7d455ce6231ec56
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2021-01-24 20:00:08 +0100

    synced measure can get access and get it's values

--- lib/models/measure/synced_measure.dart
+++ lib/models/measure/synced_measure.dart
@@ -9,17 +9,39 @@ part 'synced_measure.g.dart';
 @HiveType(typeId: 5)
 class SyncedMeasure extends Measure {
   static const String measureType = 'synced';
-  final IconData icon = Icons.sync;
+  final IconData icon = Icons.devices_other;
 
   @HiveField(4)
   String trackedHealthDataTypeName;
 
-  HealthDataType get trackedHealthDataType => HealthDataType.values.firstWhere(
-      (t) => t.toString() == 'HealthDataType.${this.trackedHealthDataType}');
+  HealthDataType get trackedHealthDataType => HealthDataType.values
+      .firstWhere((t) => t.toString() == this.trackedHealthDataTypeName);
 
   SyncedMeasure({id, name, HealthDataType healthDataType, description})
       : trackedHealthDataTypeName = healthDataType.toString(),
         super(id: id, type: measureType, name: name, description: description);
+
+  @override
+  Future<bool> get canAdd {
+    return requestAuthorization(HealthFactory());
+  }
+
+  Future<bool> requestAuthorization(healthFactory) {
+    return healthFactory.requestAuthorization([
+      this.trackedHealthDataType,
+    ]);
+  }
+
+  Future<List<HealthDataPoint>> getValue() async {
+    HealthFactory healthFactory = HealthFactory();
+    await requestAuthorization(healthFactory);
+    final now = DateTime.now();
+    DateTime startDate = DateTime(now.year, now.month, now.day, 0, 0);
+    DateTime endDate = DateTime(now.year, now.month, now.day, 23, 59);
+
+    return healthFactory.getHealthDataFromTypes(
+        startDate, endDate, [this.trackedHealthDataType]);
+  }
 
   @override
   bool get canEdit => false;
