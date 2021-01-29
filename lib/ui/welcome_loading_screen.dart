c0f103c22cadd22c9b00afdaa0b63aa4adbf876c
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2021-01-29 23:13:14 +0100

    loading screen show scheduled notification

--- lib/ui/welcome_loading_screen.dart
+++ lib/ui/welcome_loading_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:provider/provider.dart';
 import 'package:studyme/models/app_state/app_state.dart';
 import 'package:studyme/routes.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class InitialLoading extends StatefulWidget {
   @override
@@ -13,7 +16,11 @@ class InitialLoading extends StatefulWidget {
 class _InitialLoadingState extends State<InitialLoading> {
   initAppState() async {
     await _setupNotifications();
-    _showNotification();
+
+    await _configureLocalTimeZone();
+
+    //_showNotification();
+    _scheduleDailyTenAMNotification();
     await Provider.of<AppState>(context, listen: false).loadAppState();
     if (Provider.of<AppState>(context, listen: false).isEditing) {
       Navigator.pushReplacementNamed(context, Routes.creator);
@@ -35,6 +42,15 @@ class _InitialLoadingState extends State<InitialLoading> {
             iOS: IOSInitializationSettings(),
             macOS: MacOSInitializationSettings());
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    //await _configureLocalTimeZone();
+  }
+
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final currentTimeZone = await FlutterNativeTimezone.getLocalTimezone();
+    print(currentTimeZone);
+    tz.setLocalLocation(tz.getLocation(currentTimeZone));
   }
 
   @override
@@ -47,8 +63,6 @@ class _InitialLoadingState extends State<InitialLoading> {
     final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
         FlutterLocalNotificationsPlugin();
 
-    print("hi");
-
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
         AndroidNotificationDetails(
             'your channel id', 'your channel name', 'your channel description',
@@ -60,5 +74,37 @@ class _InitialLoadingState extends State<InitialLoading> {
     await flutterLocalNotificationsPlugin.show(
         10, 'plain title', 'plain body', platformChannelSpecifics,
         payload: 'item x');
+  }
+
+  Future<void> _scheduleDailyTenAMNotification() async {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'daily scheduled notification title',
+        'daily scheduled notification body',
+        _nextInstanceOfTenAM(),
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+              'daily notification channel id',
+              'daily notification channel name',
+              'daily notification description'),
+        ),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+  }
+
+  tz.TZDateTime _nextInstanceOfTenAM() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+        tz.local, now.year, now.month, now.day, now.hour, now.minute + 1);
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+    print(scheduledDate);
+    return scheduledDate;
   }
 }
