09d95274ecbe4452f63567faf86a2dccb9ba4d11
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2021-02-02 18:33:44 +0100

    add new onboarding screen

--- lib/ui/screens/onboarding.dart
+++ lib/ui/screens/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:studyme/models/app_state/app_data.dart';
 import 'package:studyme/models/app_state/app_state.dart';
@@ -6,44 +7,143 @@ import 'package:studyme/ui/widgets/intervention_letter.dart';
 
 import '../../routes.dart';
 
-class Onboarding extends StatelessWidget {
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final int _numPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _numPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.green : Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Welcome to StudyMe", style: TextStyle(fontSize: 30)),
-                  Text("Compare Interventions"),
-                  Text("StudyMe let's you evaluate interventions."),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InterventionLetter(isA: true),
-                      Text("vs."),
-                      InterventionLetter(
-                        isA: false,
-                      )
-                    ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: _navigateToCreator,
+                child: Text(
+                  'Skip',
+                  style: TextStyle(
+                    fontSize: 20.0,
                   ),
-                  Text(
-                      "An Intervention is anything you can do to reach your desired outcome."),
-                  Text(
-                      "To know if an intervention is actually working we use measures"),
-                  Text("A measure tells you if ...")
+                ),
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                physics: ClampingScrollPhysics(),
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Welcome to StudyMe",
+                            style: TextStyle(fontSize: 30)),
+                        Text("Compare Interventions"),
+                        Text("StudyMe let's you evaluate interventions."),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            InterventionLetter(isA: true),
+                            Text("vs."),
+                            InterventionLetter(
+                              isA: false,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            "An Intervention is anything you can do to reach your desired outcome."),
+                        Text(
+                            "To know if an intervention is actually working we use measures"),
+                        Text("A measure tells you if ...")
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[],
+                    ),
+                  ),
                 ],
-              ))),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Provider.of<AppData>(context, listen: false)
-              .saveAppState(AppState.CREATING);
-          Navigator.pushReplacementNamed(context, Routes.creator);
+          if (_isAtEnd()) {
+            _navigateToCreator();
+          } else {
+            _pageController.nextPage(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.ease,
+            );
+          }
         },
-        child: Icon(Icons.arrow_forward),
+        backgroundColor: _isAtEnd() ? Colors.green : Colors.blueGrey,
+        child: Icon(_isAtEnd() ? Icons.check : Icons.arrow_forward),
       ),
     );
+  }
+
+  _isAtEnd() {
+    return _currentPage == _numPages - 1;
+  }
+
+  _navigateToCreator() {
+    Provider.of<AppData>(context, listen: false)
+        .saveAppState(AppState.CREATING);
+    Navigator.pushReplacementNamed(context, Routes.creator);
   }
 }
