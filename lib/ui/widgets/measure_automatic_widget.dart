7004beb254375a81efe5502e287a8d9349ebd582
Alexander Zenner <15657532+alexanderzenner@users.noreply.github.com>
2021-03-09 13:36:47 +0100

    add hint that app needs to be reloaded for automatic meausres

--- lib/ui/widgets/measure_automatic_widget.dart
+++ lib/ui/widgets/measure_automatic_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:studyme/models/measure/automatic_measure.dart';
+import 'package:studyme/ui/widgets/hint_card.dart';
 
 class AutomaticMeasureWidget extends StatelessWidget {
   final AutomaticMeasure measure;
@@ -14,13 +15,17 @@ class AutomaticMeasureWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        if (setConfirmed != null)
-          SwitchListTile(
-            title:
-                Text("I entered the data in my Google Fit / Apple Health app"),
-            value: confirmed,
-            onChanged: setConfirmed,
-          )
+        SwitchListTile(
+          title: Text("I entered the data in my Google Fit / Apple Health app"),
+          value: confirmed,
+          onChanged: setConfirmed,
+        ),
+        SizedBox(height: 20),
+        if (confirmed)
+          HintCard(titleText: 'Restart app to fetch values', body: [
+            Text(
+                "The newest values will be automatically fetched when you restart the app")
+          ]),
       ],
     );
   }
